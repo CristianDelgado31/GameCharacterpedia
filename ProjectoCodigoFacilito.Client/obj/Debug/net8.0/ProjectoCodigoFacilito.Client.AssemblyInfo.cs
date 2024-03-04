56b4e5fc1f668e6d98a4d5d79e5a07662807927c
CristianDelgado31 <cristian-31-10@hotmail.com>
2024-03-04 11:53:15 -0300

    Solucion error ver characters sin ser user

--- ProjectoCodigoFacilito.Client/obj/Debug/net8.0/ProjectoCodigoFacilito.Client.AssemblyInfo.cs
+++ ProjectoCodigoFacilito.Client/obj/Debug/net8.0/ProjectoCodigoFacilito.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectoCodigoFacilito.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3eb26b2e4142d434ac538ffac90658cc14be1af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5db733ca004b265366b137b566972326d601610a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectoCodigoFacilito.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectoCodigoFacilito.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
